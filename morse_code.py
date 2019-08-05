b22b4f7dde137f24c86175fe744d67a78d0ed5e1
Molly Hayward <mollyxhayward@gmail.com>
2019-08-05 15:07:35 +0100

    Morse code conversion

---
+++ morse_code.py
@@ -0,0 +1,56 @@
+from re import fullmatch
+
+morse_dict = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    ".": ".-.-.-",
+    "?": "..--..",
+    ",": "--..--",
+    "!": "-.-.--"
+}
+
+# example
+# .. / .- -- / ..-. .. -. . .-.-.- / .... --- .-- / .- .-. . / -.-- --- ..- ..--..
+
+input_string = input('Enter a string: ').strip().lower()
+input_list = input_string.split()
+total_string = ''
+if fullmatch('[. -/]*', input_string):
+    for entry in input_list:
+        if entry == '/':
+            total_string += ' '
+            continue
+        for key, value in morse_dict.items():
+            if value == entry:
+                total_string += key
+else:
+    for entry in input_list:
+        for letter in entry:
+            total_string += morse_dict.get(letter) + ' '
+        total_string += '/ '
+total_string = total_string.capitalize()
+print(total_string)
